197a4c8e76785de947d268355f76ea1d350e10a5
Mohamed-A-Badr <badr49960@gmail.com>
2024-12-30 21:32:15 +0200

    Categorize apis in swagger docs and add pagination

--- products/views.py
+++ products/views.py
@@ -1,15 +1,18 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from drf_spectacular.utils import extend_schema
 
 # Create your views here.
 
 
+@extend_schema(tags=["Products"])
 class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
+@extend_schema(tags=["Products"])
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
