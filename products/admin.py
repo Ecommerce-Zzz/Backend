21506984c5dd49e61b65f3c825c103f290b58e7e
Mohamed-A-Badr <badr49960@gmail.com>
2024-12-30 05:32:57 +0200

    implement product model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
+
 
 # Register your models here.
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ["title", "owner", "price", "available", "created_at"]
+    list_filter = ["created_at", "owner", "available"]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+    owner = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
