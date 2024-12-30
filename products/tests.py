ff8055f190d29beea3591b01599beb63735a37fb
Mohamed-A-Badr <badr49960@gmail.com>
2024-12-30 19:00:29 +0200

    Add tests for list and create views

--- products/tests.py
+++ products/tests.py
@@ -1,11 +1,14 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework.test import APITestCase
 from .models import Product
 from django.contrib.auth.models import User
+from rest_framework import status
+from django.urls import reverse
 
 
 # Create your tests here.
 # * model testing
-class ProductTests(TestCase):
+class ProductTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = User.objects.create_user(username="test-user", password="123456")
@@ -16,6 +19,16 @@ class ProductTests(TestCase):
             owner=cls.user,
             available=False,
         )
+        cls.data = {
+            "title": "test",
+            "description": "test",
+            "price": 100,
+            "owner": cls.user.id,
+            "available": False,
+        }
+
+    def setUp(self):
+        self.client.login(username="test-user", password="123456")
 
     def test_user_content(self):
         self.assertEqual(self.user.username, "test-user")
@@ -26,3 +39,17 @@ class ProductTests(TestCase):
         self.assertEqual(self.product.price, 20.25)
         self.assertEqual(self.product.owner, self.user)
         self.assertEqual(self.product.available, False)
+
+    def test_list_view(self):
+        response = self.client.get(reverse("product_list_create"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertContains(response, self.product)
+
+    def test_create_view(self):
+        response = self.client.post(reverse("product_list_create"), data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 2)
+        self.assertEqual(Product.objects.first().title, "title")
+        self.assertEqual(Product.objects.last().title, "test")
+        self.assertEqual(response.json()["title"], "test")
